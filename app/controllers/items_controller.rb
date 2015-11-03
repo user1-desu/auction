df9fc674fff4fe3051f6e4351b8fb97c8eea8c2e
yatsu <user@ElitesCamp.local>
2015-11-03 19:17:42 +0900

    new周りのコントローラを実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,18 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
 end
