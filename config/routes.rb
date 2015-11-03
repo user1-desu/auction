d9e04dea8df9b652913c7fb3f16baeb0106e79ab
yatsu <user@ElitesCamp.local>
2015-11-03 22:30:49 +0900

    root 設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/items/:id' => 'items#show'
   get '/items' => 'items#index'
   post '/items' => 'items#create'
+  root 'items#index'
   # root 'items#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
