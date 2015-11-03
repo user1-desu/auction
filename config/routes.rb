944cd2ab3d86511cd1100fea7ec348c130fff261
yatsu <user@ElitesCamp.local>
2015-11-03 19:22:24 +0900

    new.html.erbの新規作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get '/items/new' => 'items#new'
   get '/items/:id' => 'items#show'
   get '/items' => 'items#index'
+  post '/items' => 'items#create'
+  # root 'items#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
